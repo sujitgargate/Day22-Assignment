40cbcd7b3a4de55acb66d802a4c85fbf9dfc9135
sujit gargate <sujitgargate5893@gmail.com>
2020-07-12 16:57:14 +0530

    Employee Daily Wage check added

--- empWageComputation.java
+++ empWageComputation.java
@@ -31,3 +31,28 @@ public class EmpPresentAbsent{
 //This is end of FIle EmpPresentAbsent //
 
 
+
+public class EmpDailyWage{
+
+	public static void main(String args[]){
+		//Constants
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		//Variables
+		int empHrs=0;
+		int empWage=0;
+		//COMPUTATION
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck==IS_FULL_TIME){
+			empHrs=8;
+		}else{
+			empHrs=0;
+		}
+		empWage=EMP_RATE_PER_HOUR*empHrs;
+		System.out.println(" Employee Wage "+empWage);
+	}
+}
+
+//This is Employee daily wage end
+
+
