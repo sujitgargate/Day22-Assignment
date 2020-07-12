8d282bbb82e7e3b42615309a4f4e379c7aec258b
sujit gargate <sujitgargate5893@gmail.com>
2020-07-12 16:54:55 +0530

    Employee present or absent added

--- empWageComputation.java
+++ empWageComputation.java
@@ -9,3 +9,25 @@ public class HelloMessage{
 
 //End of Hello Message//
 
+//This is Employee Daily Wage code
+
+
+//This is EmpPresentAbsent //
+
+public class EmpPresentAbsent{
+
+	public static void main(String args[]){
+	int IS_FULL_TIME=1;
+	double empCheck=Math.floor(Math.random()*10)%2;
+	if(empCheck==IS_FULL_TIME){
+		System.out.println("Employee is present ");
+		
+	}else{
+		System.out.println("Employee is absent ");
+	}
+	}
+}
+
+//This is end of FIle EmpPresentAbsent //
+
+
