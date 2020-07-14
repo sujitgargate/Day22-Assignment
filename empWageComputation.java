0e39272e954dde0dd47b409e1fe17cd5eaad19d9
sujitgargate <sujitgargate5893@gmail.com>
2020-07-14 19:42:28 +0530

    Employee Part Full time Wage

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,20 +1,25 @@
+//This is Part_Full time  //
 
-//This is Employee Daily Wage code
-
-public class EmpDailyWage{
+public class EmpPartFullTime{
 
 	public static void main(String args[]){
 		//Constants
 		int IS_FULL_TIME=1;
+		int IS_PART_TIME=2;
 		int EMP_RATE_PER_HOUR=20;
 		//Variables
 		int empHrs=0;
 		int empWage=0;
 		//COMPUTATION
-		double empCheck=Math.floor(Math.random()*10)%2;
+		double empCheck=Math.floor(Math.random()*10)%3;
 		if(empCheck==IS_FULL_TIME){
 			empHrs=8;
-		}else{
+
+		}else if(empCheck==IS_PART_TIME){
+                        empHrs=4;
+                }
+
+		else{
 			empHrs=0;
 		}
 		empWage=EMP_RATE_PER_HOUR*empHrs;
@@ -22,4 +27,4 @@ public class EmpDailyWage{
 	}
 }
 
-//This is Employee daily wage end
+//This is end of Part_Full time  //
